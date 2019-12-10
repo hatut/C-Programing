89fbc53de635eec5779b72b46b89934cbf024d0d
hatut <hatut@foxmail.com>
2019-12-10 17:22:56 +0800

    ko no dio da

---
+++ Week17/[Arrays]1108. 纸牌游戏_hzh.c
@@ -0,0 +1,23 @@
+//It's ╤сап 
+#include<stdio.h>
+#include<string.h>
+int b[10000];
+int i,j,k,n,m=0;
+int main()
+{
+	scanf("%d",&m);
+	while(m){
+		scanf("%d",&n);
+		for(i=1;i<=n;i++)
+			b[i]=i;
+		int h=1,e=n;
+		while(n){
+		printf("%d ",b[h]);
+		 h+=2;e+=1;
+		 b[e]=b[h-1];
+		 n--;
+		}
+		printf("\n");
+		m--;
+	}
+}
